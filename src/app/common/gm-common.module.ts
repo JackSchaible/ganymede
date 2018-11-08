36bdbfeacb2101fb1e5ddb44d4b00b4c4b781358
Jack Schaible <jack.schaible@hotmail.com>
2018-11-08 00:23:13 -0700

    Added a simple processing overlay

--- src/app/common/gm-common.module.ts
+++ src/app/common/gm-common.module.ts
@@ -2,16 +2,32 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { MonsterCardComponent } from "./monster-card/monster-card.component";
 import { AlignmentTableComponent } from "./alignment-table/alignment-table.component";
-import { MatGridListModule, MatButtonModule } from "@angular/material";
+import {
+	MatGridListModule,
+	MatButtonModule,
+	MatProgressSpinnerModule
+} from "@angular/material";
 import { SpellCardComponent } from "./spell-card/spell-card.component";
+import { ProcessingOverlayComponent } from "./processing-overlay/processing-overlay.component";
 
 @NgModule({
-	imports: [CommonModule, MatGridListModule, MatButtonModule],
+	imports: [
+		CommonModule,
+		MatGridListModule,
+		MatButtonModule,
+		MatProgressSpinnerModule
+	],
 	declarations: [
 		MonsterCardComponent,
 		AlignmentTableComponent,
-		SpellCardComponent
+		SpellCardComponent,
+		ProcessingOverlayComponent
 	],
-	exports: [SpellCardComponent, MonsterCardComponent, AlignmentTableComponent]
+	exports: [
+		SpellCardComponent,
+		MonsterCardComponent,
+		AlignmentTableComponent,
+		ProcessingOverlayComponent
+	]
 })
 export class GmCommonModule {}
