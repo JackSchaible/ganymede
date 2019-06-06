8e0045ecd520cbf0b1feda3097ad19f83bd59142
Jack.Schaible <jack.schaible@ama.ab.ca>
2019-06-06 16:59:00 -0600

    Added model classes for modal #50

---
+++ src/app/common/models/buttonModel.ts
@@ -0,0 +1,6 @@
+export class ButtonModel {
+	public icon: string;
+	public color: string;
+	public titleText: string;
+	public onClick: () => void;
+}
---
+++ src/app/common/models/modalModel.ts
@@ -0,0 +1,8 @@
+import { ButtonModel } from "./buttonModel";
+
+export class ModalModel {
+	public title: string;
+	public content: string;
+	public closeButton: ButtonModel;
+	public buttons: ButtonModel[];
+}
