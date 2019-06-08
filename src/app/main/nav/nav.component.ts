5384932f90f20e820ca11dfd9465b2f256eb32f3
Jack Schaible <jack.schaible@hotmail.com>
2019-06-07 23:37:44 -0600

    Update nav to allow for different states when selecting a campaign #56

--- src/app/main/nav/nav.component.ts
+++ src/app/main/nav/nav.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from "@angular/core";
 import { AuthService } from "../../services/auth.service";
 import { DeviceDetectorService } from "ngx-device-detector";
-import { Router } from "@angular/router";
+import { Router, NavigationEnd, RouterEvent } from "@angular/router";
 import NavItem from "../../models/nav/navItem";
 import { Md5 } from "ts-md5/dist/md5";
+import { filter } from "rxjs/operators";
 
 @Component({
 	selector: "gm-nav",
@@ -18,6 +19,9 @@ export class NavComponent implements OnInit {
 	userHash: string;
 
 	items: NavItem[];
+	campaignItems: NavItem[];
+
+	currentItems: NavItem[];
 
 	constructor(
 		private authService: AuthService,
@@ -39,8 +43,13 @@ export class NavComponent implements OnInit {
 
 	ngOnInit(): void {
 		this.user = this.getUsername();
-
 		if (this.user) this.userHash = Md5.hashStr(this.user) as string;
+
+		this.router.events
+			.pipe(filter((event: RouterEvent) => event instanceof NavigationEnd))
+			.subscribe((event: NavigationEnd) =>
+				this.configureItems(event.urlAfterRedirects)
+			);
 	}
 
 	getUsername(): string {
@@ -58,5 +67,34 @@ export class NavComponent implements OnInit {
 
 	isLoggedIn(): boolean {
 		return !!this.authService.getUser();
+	}
+
+	private configureItems(url: string) {
+		const matches = url.match(/campaign\/[0-9]+/g);
+
+		if (matches && matches.length > 0) {
+			const searchStr = "campaign/";
+			const match: string = matches[0];
+			const campaignIndex = match.indexOf(searchStr) + searchStr.length;
+			if (campaignIndex >= 0) {
+				const id = parseInt(
+					match.substr(campaignIndex, match.length - campaignIndex),
+					10
+				);
+				this.campaignItems = [
+					new NavItem("", "fab fa-d-and-d", "DM Tools", true),
+					new NavItem(
+						`campaign/${id}`,
+						"fas fa-scroll",
+						"The Campaign",
+						false,
+						[new NavItem(`campaign/edit/${id}`, "fas fa-pencil", "Edit")]
+					),
+					new NavItem(`campaign/${id}/monsters`, "fas fa-paw-claws", "Monsters")
+				];
+
+				this.currentItems = this.campaignItems;
+			}
+		} else this.currentItems = this.items;
 	}
 }
